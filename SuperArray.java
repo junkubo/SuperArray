37a143eea5dbff81090d77ed4699a3f4ee5d6141
Jun <jkubo20@stuy.edu>
2020-11-01 21:01:21 -0500

    set, resize, add

--- SuperArray.java
+++ SuperArray.java
@@ -8,11 +8,26 @@ class SuperArray {
   public int size() {
     return size;
   }
-  public boolean add(String element) {
-    data[size] = element;
-    size += 1;
-  }
   public String get(int index) {
     return data[index];
+  }
+  public String set(int index, String element) {
+    i = data[index];
+    data[index] = element;
+    return i;
+  }
+  private void resize() {
+    data1 = String[data.length + 1];
+    for (int i = 0; i < data.length; i++) {
+      data1[i] = data[i];
+    }
+    data = data1;
+  }
+  public boolean add(String element) {
+    if (size == data.length) {
+      resize();
+    }
+    data[size] = element;
+    size += 1;
   }
 }
